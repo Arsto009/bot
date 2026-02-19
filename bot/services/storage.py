7f638df435f11e200592377f42cfcf0e39dc5735
Arsto009 <saso.pubg@gmail.com>
2026-02-19 21:44:11 +0300

    Add files via upload

---
+++ bot/services/storage.py
@@ -0,0 +1,14 @@
+import json
+import os
+
+DB_FILE = os.path.join("data", "listings.json")
+
+
+def load_data():
+    with open(DB_FILE, "r", encoding="utf-8") as f:
+        return json.load(f)
+
+
+def save_data(data):
+    with open(DB_FILE, "w", encoding="utf-8") as f:
+        json.dump(data, f, ensure_ascii=False, indent=2)

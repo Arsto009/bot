2ec0351aebc1e2fee8379ae903b49fa431449f04
Arsto009 <saso.pubg@gmail.com>
2026-02-20 14:59:16 +0300

    Fix formatting issues in settings.py

--- bot/settings.py
+++ bot/settings.py
@@ -16,7 +16,7 @@ def load_config():
 
         default_config = {
 
-            "ADMIN_ID": "",
+            "ADMIN_ID": ,
 
             "BOT_TOKEN": "",
 
@@ -66,3 +66,4 @@ def is_admin(user_id):
 
 
     )
+
